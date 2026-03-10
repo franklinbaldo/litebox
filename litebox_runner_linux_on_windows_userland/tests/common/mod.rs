9f35b067130009df0267fc250ba4249252dcfb61
Weiteng Chen <weitengchen@microsoft.com>
2026-03-09 20:41:47 -0700

    fix Windows

--- litebox_runner_linux_on_windows_userland/tests/common/mod.rs
+++ litebox_runner_linux_on_windows_userland/tests/common/mod.rs
@@ -10,7 +10,7 @@ use litebox_platform_multiplex::Platform;
 
 pub struct TestLauncher {
     platform: &'static Platform,
-    shim_builder: litebox_shim_linux::LinuxShimBuilder<litebox_shim_linux::DefaultFS>,
+    shim_builder: litebox_shim_linux::LinuxShimBuilder,
     fs: litebox_shim_linux::DefaultFS,
 }
 
@@ -75,7 +75,7 @@ impl TestLauncher {
         self.fs.close(&fd).unwrap();
     }
 
-    pub fn test_load_exec_common(mut self, executable_path: &str) {
+    pub fn test_load_exec_common(self, executable_path: &str) {
         let fs = std::sync::Arc::new(self.fs);
         let argv = vec![
             CString::new(executable_path).unwrap(),
