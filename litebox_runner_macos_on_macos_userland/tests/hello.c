5282985b9953c07bf3ae4c444df6bee192748f74
Sangho Lee <sanghle@Weidongs-Mini.guest.corp.microsoft.com>
2026-04-04 13:29:35 -0700

    test: enhance macOS hello.c with envp printing and clock_gettime

--- litebox_runner_macos_on_macos_userland/tests/hello.c
+++ litebox_runner_macos_on_macos_userland/tests/hello.c
@@ -2,10 +2,35 @@
 // Licensed under the MIT license.
 
 #include <stdio.h>
+#include <time.h>
 
-int main(int argc, char *argv[]) {
-    for (int i = 0; i < argc; i++) {
+void test_clock_gettime() {
+    struct timespec start, end;
+    if (clock_gettime(CLOCK_MONOTONIC, &start) == -1) {
+        perror("clock_gettime failed for start time");
+        return;
+    }
+    // Do some work
+    for (volatile int i = 0; i < 1000000; i++);
+    if (clock_gettime(CLOCK_MONOTONIC, &end) == -1) {
+        perror("clock_gettime failed for end time");
+        return;
+    }
+    double elapsed = (end.tv_sec - start.tv_sec) + (end.tv_nsec - start.tv_nsec) / 1e9;
+    printf("Elapsed time: %f seconds\n", elapsed);
+}
+
+int main(int argc, char *argv[], char *envp[]) {
+    int i;
+    for (i = 0; i < argc; i++) {
         printf("argv[%d] = %s\n", i, argv[i]);
     }
+
+    for (i = 0; envp[i] != NULL; i++) {
+        printf("envp[%d] = %s\n", i, envp[i]);
+    }
+
+    test_clock_gettime();
+
     return 0;
 }
