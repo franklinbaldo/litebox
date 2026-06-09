00e964df2602e2a03d4cf535b7aa9fd7b6c98d4e
Weiteng Chen <weitengchen@microsoft.com>
2026-06-09 13:16:50 -0700

    fix apphelp

--- litebox_shim_windows/src/syscalls/apphelp.rs
+++ litebox_shim_windows/src/syscalls/apphelp.rs
@@ -32,14 +32,10 @@ pub(crate) fn sys_nt_apphelp_cache_control<Platform: ShimPlatform>(
 
     let status = match service_class {
         ApphelpCacheServiceClass::Lookup
+        | ApphelpCacheServiceClass::LookupCdb
         | ApphelpCacheServiceClass::Remove
-        | ApphelpCacheServiceClass::Update
-        | ApphelpCacheServiceClass::LookupCdb => {
-            if service_data.is_some() {
-                NtStatus::SUCCESS
-            } else {
-                NtStatus::INVALID_PARAMETER
-            }
+        | ApphelpCacheServiceClass::Update => {
+            service_data.map_or(NtStatus::INVALID_PARAMETER, |_| NtStatus::SUCCESS)
         }
         ApphelpCacheServiceClass::Flush
         | ApphelpCacheServiceClass::Dump
@@ -66,24 +62,24 @@ mod tests {
         service_class as u32
     }
 
-    fn service_data_ptr(service_data: &mut u8) -> MutPtr<TestPlatform, u8> {
-        MutPtr::<TestPlatform, u8>::from_ptr(core::ptr::from_mut(service_data))
+    fn service_data_ptr() -> MutPtr<TestPlatform, u8> {
+        MutPtr::<TestPlatform, u8>::from_ptr(core::ptr::NonNull::dangling().as_ptr())
     }
 
     #[test]
     fn nt_apphelp_cache_control_accepts_empty_cache_lookups() {
-        let mut service_data = 0;
         assert_eq!(
             sys_nt_apphelp_cache_control::<TestPlatform>(
                 service_value(ApphelpCacheServiceClass::Lookup),
-                Some(service_data_ptr(&mut service_data)),
+                Some(service_data_ptr()),
             ),
             NtStatus::SUCCESS
         );
+
         assert_eq!(
             sys_nt_apphelp_cache_control::<TestPlatform>(
                 service_value(ApphelpCacheServiceClass::LookupCdb),
-                Some(service_data_ptr(&mut service_data)),
+                Some(service_data_ptr()),
             ),
             NtStatus::SUCCESS
         );
@@ -91,11 +87,10 @@ mod tests {
 
     #[test]
     fn nt_apphelp_cache_control_accepts_cache_mutations_as_noops() {
-        let mut service_data = 0;
         assert_eq!(
             sys_nt_apphelp_cache_control::<TestPlatform>(
                 service_value(ApphelpCacheServiceClass::Update),
-                Some(service_data_ptr(&mut service_data)),
+                Some(service_data_ptr()),
             ),
             NtStatus::SUCCESS
         );
@@ -118,12 +113,8 @@ mod tests {
             NtStatus::INVALID_PARAMETER
         );
 
-        let mut service_data = 0;
         assert_eq!(
-            sys_nt_apphelp_cache_control::<TestPlatform>(
-                u32::MAX,
-                Some(service_data_ptr(&mut service_data)),
-            ),
+            sys_nt_apphelp_cache_control::<TestPlatform>(u32::MAX, Some(service_data_ptr()),),
             NtStatus::INVALID_PARAMETER
         );
     }
