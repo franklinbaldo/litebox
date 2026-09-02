10272963696d83ab4003646a5e03144b4f9089e4
Franklin Silveira Baldo <franklinbaldo@gmail.com>
2026-09-02 17:08:52 -0400

    test(android): add headless ART fixture

---
+++ fixtures/android/ArtHello.java
@@ -0,0 +1,9 @@
+package dev.litebox;
+
+public final class ArtHello {
+    private ArtHello() {}
+
+    public static void main(String[] args) {
+        System.out.println("hello from ART on LiteBox");
+    }
+}
