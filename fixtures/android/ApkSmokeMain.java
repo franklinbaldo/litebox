2d62eaa30c6eb66d506562a471ac53d41dac5f59
Franklin Silveira Baldo <franklinbaldo@gmail.com>
2026-09-02 17:37:28 -0400

    test(android): add APK smoke main fixture

---
+++ fixtures/android/ApkSmokeMain.java
@@ -0,0 +1,9 @@
+package dev.litebox.androidsmoke;
+
+public final class ApkSmokeMain {
+    private ApkSmokeMain() {}
+
+    public static void main(String[] args) {
+        System.out.println("hello from APK on LiteBox");
+    }
+}
